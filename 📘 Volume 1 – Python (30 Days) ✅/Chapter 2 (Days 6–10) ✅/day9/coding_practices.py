e024c3dc0811670fd65cc09697240835ac7be5d8
akshata-beep <akshatanelagudda22@gmail.com>
2026-07-22 23:39:03 +0530

    day9 of day6
    code with me bro

---
+++ 📘 Volume 1 – Python (30 Days) ✅/Chapter 2 (Days 6–10) ✅/day9/coding_practices.py
@@ -0,0 +1,18 @@
+name = "Akshata"
+age = 22
+cgpa = 9.8
+placed = False
+company = None
+# print(name,age,cgpa,placed,company,sep="\n")
+# print(type(name),type(age),type(cgpa),type(company),sep="\n")
+
+
+# ===== STUDENT INFO =====
+# Name    : Akshata
+# Age     : 22
+# CGPA    : 9.8
+# Placed  : False
+# Company : None
+# ========================
+
+print("===== STUDENT INFO =====","\nName    : ",name,"\nAge     : ",age,"\nCGPA    : ",cgpa,"\nPlaced  : ",placed,"\nCompany : ",company,"\n========================")
---
+++ 📘 Volume 1 – Python (30 Days) ✅/Chapter 2 (Days 6–10) ✅/day9/coding_practies2.py
@@ -0,0 +1,15 @@
+employee_name = "Akshata"
+employee_id = 101
+salary = 55000.50
+is_permanent = True
+department = None
+
+print(f"========== EMPLOYEE DETAILS ==========\n"
+      f"Name        : {employee_name}\n"
+      f"ID          : {employee_id}\n"
+      f"Salary      : {salary}\n"
+      f"Permanent   : {is_permanent}\n"
+      f"Department  : {department}\n\n"f"Data Types\nName        : {type(employee_name)}\n"f"ID          : {type(employee_id)}\n"f"Salary      : {type(salary)}\n"f"Permanent   : {type(is_permanent)}\n"f"Department  : {type(department)}\n"f"======================================")
+
+
+print(type(True))
